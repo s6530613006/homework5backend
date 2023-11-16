7a1ba7f6c3074f0171ca4714dff2844f9da3df3c
s6530613006 <kuroomi10223@gmail.com>
2023-11-16 11:25:08 +0700

    second

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -8,5 +8,6 @@ module.exports = (app)=>{
     router.get("/", authJwt, user_controller.getAllUsers);
     router.put("/:id", authJwt, user_controller.updateUserCtrl);
     router.delete("/:id/:p1/:p2", authJwt, user_controller.deleteUser);
+    router.delete("/:id", authJwt, user_controller.deleteUser);
     app.use("/api/auth", router);
 };
